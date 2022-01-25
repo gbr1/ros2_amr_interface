0e6fb0085a968850455bb4a541c6fced02dd6436
gbr1 <giovannididio.bruno@gmail.com>
2022-01-25 23:00:20 +0000

    now publisher is moved as class method

--- src/amr_interface_node.cpp
+++ src/amr_interface_node.cpp
@@ -21,6 +21,8 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+#include <chrono>
+#include <functional>
 #include <memory>
 #include <string>
 #include <unistd.h>
@@ -38,11 +40,12 @@
 #include "ros2_amr_interface/FIKmodel.hpp"
 #include "ucPack/ucPack.h"
 
+using namespace std::chrono_literals;
 
-std::string port_name="/dev/ttyUSB0";
+
+std::string port_name="/dev/ttyACM0";//USB0";
 
 FIKmodel mecanum(0.0825,0.105,0.04); //lx=0.0825m, ly=0.105m , r= 0.04m
-//FIKmodel mecanum(0.165,0.21,0.04);
 ucPack packeter(100);
 
 class AMR_Node: public rclcpp::Node{
@@ -55,10 +58,11 @@ class AMR_Node: public rclcpp::Node{
       
       rclcpp::Time previous_time;
 
-
+      rclcpp::TimerBase::SharedPtr odom_timer;
       
       rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr joy_subscription;
       rclcpp::Subscription<geometry_msgs::msg::PoseWithCovariance>::SharedPtr initial_pose_subscription;
+      rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_publisher;
 
 
 
@@ -119,6 +123,53 @@ class AMR_Node: public rclcpp::Node{
         }
       }
 
+      void odom_pub_callback(){
+        rclcpp::Time now = this->get_clock()->now();
+        geometry_msgs::msg::TransformStamped t;
+
+        t.header.stamp = now;
+        t.header.frame_id = "odom";
+        t.child_frame_id = "base_link";
+
+        t.transform.translation.x = this->x;
+        t.transform.translation.y = this->y;
+        t.transform.translation.z = 0.0;
+
+        tf2::Quaternion q;
+        q.setRPY(0.0,0.0,this->theta);
+        t.transform.rotation.x = q.x();
+        t.transform.rotation.y = q.y();
+        t.transform.rotation.z = q.z();
+        t.transform.rotation.w = q.w();
+
+        this->tf_bc->sendTransform(t);
+        
+        nav_msgs::msg::Odometry odom;
+        odom.header.stamp=now;
+        odom.header.frame_id="odom";
+        odom.child_frame_id="base_link";
+        odom.pose.pose.position.x=this->x;
+        odom.pose.pose.position.y=this->y;
+        odom.pose.pose.position.z=0.0;
+        odom.pose.pose.orientation.x=q.x();
+        odom.pose.pose.orientation.y=q.y();
+        odom.pose.pose.orientation.z=q.z();
+        odom.pose.pose.orientation.w=q.w();
+        odom.child_frame_id="base_link";
+        odom.twist.twist.linear.x = this->vx;
+        odom.twist.twist.linear.y = this->vy;
+        odom.twist.twist.linear.z = 0.0;
+        odom.twist.twist.angular.x=0.0;
+        odom.twist.twist.angular.y=0.0;
+        odom.twist.twist.angular.z=this->w;
+
+        odom_publisher->publish(odom);
+
+        vx=0.0;
+        vy=0.0;
+        w=0.0;
+      }
+
 
 
 
@@ -160,6 +211,9 @@ class AMR_Node: public rclcpp::Node{
 
       joy_subscription = this->create_subscription<geometry_msgs::msg::Twist>("/cmd_vel",1,std::bind(&AMR_Node::joy_callback, this, std::placeholders::_1));
       initial_pose_subscription = this->create_subscription<geometry_msgs::msg::PoseWithCovariance>("/amr/initial_pose",1,std::bind(&AMR_Node::initial_pose_callback, this, std::placeholders::_1));
+      odom_publisher = this->create_publisher<nav_msgs::msg::Odometry>("/wheel/odometry",1);
+      odom_timer = this->create_wall_timer(10ms, std::bind(&AMR_Node::odom_pub_callback, this));
+
     }
 
     ~AMR_Node(){
@@ -178,67 +232,7 @@ class AMR_Node: public rclcpp::Node{
 
 int main(int argc, char ** argv){
   rclcpp::init(argc, argv);
-  //rclcpp::spin(std::make_shared<AMR_Node>());
-
-
-  auto amr_node = std::make_shared<AMR_Node>();
-  auto odom_publisher = amr_node->create_publisher<nav_msgs::msg::Odometry>("/wheel/odometry",rclcpp::QoS{1});
-  rclcpp::Rate loop_rate(100); //50hz
-
-
-
-  while(rclcpp::ok()){
-    if (amr_node->to_be_publish){
-
-      rclcpp::Time now = amr_node->get_clock()->now();
-      geometry_msgs::msg::TransformStamped t;
-
-      t.header.stamp = now;
-      t.header.frame_id = "odom";
-      t.child_frame_id = "base_link";
-
-      t.transform.translation.x = amr_node->x;
-      t.transform.translation.y = amr_node->y;
-      t.transform.translation.z = 0.0;
-
-      tf2::Quaternion q;
-      q.setRPY(0.0,0.0,amr_node->theta);
-      t.transform.rotation.x = q.x();
-      t.transform.rotation.y = q.y();
-      t.transform.rotation.z = q.z();
-      t.transform.rotation.w = q.w();
-
-      amr_node->tf_bc->sendTransform(t);
-      
-
-
-
-      nav_msgs::msg::Odometry odom;
-      odom.header.stamp=now;
-      odom.header.frame_id="odom";
-      odom.child_frame_id="base_link";
-      odom.pose.pose.position.x=amr_node->x;
-      odom.pose.pose.position.y=amr_node->y;
-      odom.pose.pose.position.z=0.0;
-      odom.pose.pose.orientation.x=q.x();
-      odom.pose.pose.orientation.y=q.y();
-      odom.pose.pose.orientation.z=q.z();
-      odom.pose.pose.orientation.w=q.w();
-      odom.child_frame_id="base_link";
-      odom.twist.twist.linear.x = amr_node->vx;
-      odom.twist.twist.linear.y = amr_node->vy;
-      odom.twist.twist.linear.z = 0.0;
-      odom.twist.twist.angular.x=0.0;
-      odom.twist.twist.angular.y=0.0;
-      odom.twist.twist.angular.z=amr_node->w;
-
-      odom_publisher->publish(odom);
-      amr_node->to_be_publish = false;
-    }
-    rclcpp::spin_some(amr_node);
-    loop_rate.sleep();
-  }
-
+  rclcpp::spin(std::make_shared<AMR_Node>());
   rclcpp::shutdown();
   return 0;
 }
